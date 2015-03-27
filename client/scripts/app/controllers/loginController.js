1a977f239e86949d6335cde31b9c7d76b7b21dc5
Deretor <zarew1993@yandex.ru>
2015-03-27 13:01:13 +0300

    add login page and reg fuinction

--- client/scripts/app/controllers/loginController.js
+++ client/scripts/app/controllers/loginController.js
@@ -4,23 +4,39 @@
 
 (function (){
     var app = angular.module('app');
-
-    app.controller = ('loginController',['$scope','loginService', contrFunction]);
+    app.controller('loginController', ['$scope', 'loginService', contrFunction]);
 
     function contrFunction($scope, loginService){
+        var vm=this;
+        vm.modeAr=['login','registr'];
+        vm.Vmode = vm.modeAr[0];
 
-        var addUser = function(user){
-            loginRepository.addUser(user);
+        vm.addUser = function(){
+            var user = {
+                group: vm.regGr,
+                login : vm.regLog,
+                password : vm.regPass
+
+            };
+            loginService.addUser(user);
         };
-        var testUser = function(user){
-            return  loginRepository.testUser(user);
+        var testUser = function(){
+            var user = {
+                login : vm.loginLog,
+                password : vm.loginPass
+            };
+           loginService.testUser(user);
+            console.log(user);
         };
 
-        return  {
-            addUser : addUser,
-            testUser : testUser
-        }
-
+        vm.toReg = function(){
+            if(vm.Vmode == vm.modeAr[0])
+            {
+                vm.Vmode = vm.modeAr[1];
+            }else{
+                vm.Vmode = vm.modeAr[0];
+            }
+        };
     }
 
 })();
